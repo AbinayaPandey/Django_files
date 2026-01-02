3169a26c6596c31ecaeec214fb978bfa38655a62
Abinaya <abinayapandey360@gmail.com>
2026-01-02 15:29:30 +0545

    eight file

---
+++ ch8/student/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from student.models import profile
+
+
+def all_data(req):
+    all_students = profile.objects.all()
+    print(all_students)
+    return render(req, "student/all.html", {"students": all_students})
+
+
+def single_data(req):
+    student = profile.objects.get(pk=1)
+    print(student)
+    return render(req, "student/single.html", {"student": student})
